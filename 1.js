c9b9c82138db1ef414dcc700173fe0d55235b259
exluse <114081542+gloviee@users.noreply.github.com>
2024-12-29 00:10:33 +0200

    Update 1.js
    edited

--- 1.js
+++ 1.js
@@ -1,30 +1,50 @@
 const numbers = [1, 2, 3, 4, 5];
 
-function asyncMap(array, asyncCallback, callback) {
-  const promises = array.map((item, index) => asyncCallback(item, index, array));
-  Promise.all(promises)
-    .then((results) => callback(null, results))
-    .catch((error) => callback(error, null));
-}
 
+function asyncMap(array, asyncCallback, finalCallback) {
+  const results = [];
+  let pending = array.length;
 
-function asyncMultiply(number, index, array) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(number * index * 2);
-    }, 100 * index);
+  if (array.length === 0) {
+    finalCallback(null, results);
+    return;
+  }
+
+  array.forEach((item, index) => {
+    asyncCallback(item, index, array, (err, result) => {
+      if (err) {
+        finalCallback(err);
+        return;
+      }
+
+      results[index] = result;
+      pending--;
+
+      if (pending === 0) {
+        finalCallback(null, results);
+      }
+    });
   });
 }
 
 
-asyncMap(
-  numbers,
-  asyncMultiply,
-  (error, results) => {
-    if (error) {
-      console.error("Error during async map operation:", error);
+function doubleAsync(num, index, array, callback) {
+  setTimeout(() => {
+    if (num < 0) {
+      callback(new Error("Negative numbers are not allowed"));
     } else {
-      console.log("Async map results:", results);
+      callback(null, num * 2);
     }
+  }, 100);
+}
+
+
+function handleResults(err, results) {
+  if (err) {
+    console.error("Error:", err.message);
+  } else {
+    console.log("Results:", results);
   }
-);
+}
+
+asyncMap(numbers, doubleAsync, handleResults);
